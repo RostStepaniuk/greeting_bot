a5ad56206dc2a857a030052f447c062cb858a302
RostStepaniuk <stepanukrostislav323@gmail.com>
2024-01-21 17:43:15 +0200

    commition

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     welcome_message = "Вітаю! Ось основні правила групи: ..."
@@ -11,11 +15,11 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
     if query.data == "accept_rules":
-        group_link = "https://t.me/joinchat/YourGroupLink"
+        group_link = os.environ.get('GROUP_LINK')
         await context.bot.send_message(chat_id=query.from_user.id, text=f"Ось посилання на групу: {group_link}")
 
 def main():
-    TOKEN = "6816938238:AAG0oJhK3Y4G_bgGZmmu8vO_6Qs46suQmHo"
+    TOKEN = os.environ.get('TOKEN')
     application = Application.builder().token(TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
@@ -30,3 +34,4 @@ if __name__ == '__main__':
 
 
 #TOKEN = "6816938238:AAG0oJhK3Y4G_bgGZmmu8vO_6Qs46suQmHo"
+    
